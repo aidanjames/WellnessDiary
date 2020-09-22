aabc929adce373ad1cc303d5881a233a67494877
AidanJP <aidanpendlebury@gmail.com>
2020-09-22 17:21:07 +0100

    Initial Commit

---
+++ WellnessDiary/WellnessDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessDiaryApp.swift
+//  WellnessDiary
+//
+//  Created by Aidan Pendlebury on 22/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct WellnessDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
